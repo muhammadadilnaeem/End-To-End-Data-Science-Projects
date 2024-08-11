b21d30a03dae9b31edb28ea89ed31a6f71982745
adil <madilnaeem0@gmail.com>
2024-08-11 16:07:39 +0500

    main file of this project

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-from src.Machine_Learning_Project.logger import logging
+import sys
+from src.mlproject.logger import logging
+from src.mlproject.exception import CustomException
+from src.mlproject.components.data_ingestion import DataIngestion
+
+if __name__ == "__main__":
+    logging.info("Logging has been set up.")
+
+    try:
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+
+        logging.info("Data ingestion has been completed.")
+    except Exception as e:
+        logging.info("An error occurred during data ingestion.")
+        raise CustomException(e, sys)
