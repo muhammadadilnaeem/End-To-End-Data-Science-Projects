9640107b4528ab4a7dec225b2585311a873b1f51
adil <madilnaeem0@gmail.com>
2024-08-12 07:54:41 +0500

    the utils.py file in our project plays a supportive role by containing utility functions that are commonly used across different modules.

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -6,6 +6,9 @@ from dataclasses import dataclass
 import pandas as pd
 import pymysql
 
+import pickle
+
+
 # Setting up environment variables
 from dotenv import load_dotenv
 load_dotenv()
@@ -31,4 +34,15 @@ def read_sql_data():
         return sql_df
 
     except Exception as ex:
-        raise CustomException(str(ex), sys)
+        raise CustomException(ex)
+    
+# Function to save Pickle file
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
