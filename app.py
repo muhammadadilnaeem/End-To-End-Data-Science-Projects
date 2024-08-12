6b07da82a24abf06d3c72726d9f3dee611a7904f
adil <madilnaeem0@gmail.com>
2024-08-12 07:49:46 +0500

    main file of this project

--- app.py
+++ app.py
@@ -2,15 +2,20 @@ import sys
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_transformation import DataTransformation
 
 if __name__ == "__main__":
     logging.info("Logging has been set up.")
 
     try:
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
-
+        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
         logging.info("Data ingestion has been completed.")
+
+        data_transformation = DataTransformation()
+        data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        logging.info("Data transformation has been completed.")
+
     except Exception as e:
         logging.info("An error occurred during data ingestion.")
         raise CustomException(e, sys)
